76f0ea9eaf528ccc1dfdd8e28eb3a0cf88bfd5b0
JordanMiranda <jordan.oliveira.m@gmail.com>
2025-09-22 08:27:23 -0300

    written automatization code

---
+++ Atomatization.py
@@ -0,0 +1,63 @@
+import pyautogui as pa
+import time
+
+pa.PAUSE = 0.25
+
+pa.hotkey('command', 'tab')
+
+pa.write("155")
+for i in range (1,10):
+    pa.press('enter')
+
+
+pa.write("5147")
+for i in range (1,10):
+    pa.press('enter')
+
+pa.write("108")
+for i in range (1,10):
+    pa.press('enter')
+
+
+pa.write("40")
+for i in range (1,10):
+    pa.press('enter')
+
+pa.write("8987")
+for i in range (1,10):
+    pa.press('enter')
+
+
+pa.write("8979")
+for i in range (1,10):
+    pa.press('enter')
+    
+
+pa.write("8978")
+for i in range (1,10):
+    pa.press('enter')
+    
+
+
+pa.write("8992")
+for i in range (1,10):
+    pa.press('enter')
+    
+
+pa.write("8991")
+for i in range (1,10):
+    pa.press('enter')
+    
+
+pa.write("8966")
+for i in range (1,10):
+    pa.press('enter')
+
+pa.write("8982")
+for i in range (1,10):
+    pa.press('enter')
+
+
+pa.write("9100")
+for i in range (1,9):
+    pa.press('enter')
